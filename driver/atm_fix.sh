4a37022207efca65ff110b073816d9f4bc151e35
Parker MacCready <p.maccready@gmail.com>
2015-10-06 14:45:35 -0700

    work on am_fix

--- driver/atm_fix.sh
+++ driver/atm_fix.sh
@@ -3,15 +3,64 @@
 # This fills in gaps in our WRF inventory.
 # Designed to be run on fjord
 
-dir0="/pmraid3/darr/tstwrf/tmpwrf/"
+dir00="/pmraid3/darr/tstwrf/tmpwrf/"
+dmissing="TEST_2014010700"
+mkdir $dmissing
+cd $dir00
 
-d1="2014010612"
-dmissing="2014010700"
-d2="2014010712"
+dir1="2014010612" # get f12 to f24 and rename as f00 to f12 in dmissing
+nout=0
+while [ $nout -le 12 ]
+do
+  
+  if [ $nout -le 9 ] ; then
+    pad="0"
+  else
+    pad=""
+  fi
+  outfile=$dmissing"/wrfout.ocean_d2."$dmissing".f"$pad$nout".0000"
+  
+  nin=$[$nout + 12]
+  if [ $nin -le 9 ] ; then
+    pad="0"
+  else
+    pad=""
+  fi
+  infile=$dir1"/wrfout.ocean_d2."$dir1".f"$pad$nin".0000"
+  
+  echo "copying "$infile" to "$outfile
+  
+  cp $infile $outfile
+  
+  nout=$[$nout + 1]
+  
+done
 
-cd $dir0
-
-echo "d1"
-ls $d1
-echo "d2"
-ls $d2
+dir1="2014010712" # get f01 to f12 and rename as f13 to f24 in dmissing
+nout=13
+while [ $nout -le 24 ]
+do
+  
+  if [ $nout -le 9 ] ; then
+    pad="0"
+  else
+    pad=""
+  fi
+  outfile=$dmissing"/wrfout.ocean_d2."$dmissing".f"$pad$nout".0000"
+  
+  nin=$[$nout - 12]
+  if [ $nin -le 9 ] ; then
+    pad="0"
+  else
+    pad=""
+  fi
+  infile=$dir1"/wrfout.ocean_d2."$dir1".f"$pad$nin".0000"
+  
+  echo "copying "$infile" to "$outfile
+  
+  #cp $infile $outfile
+  
+  nout=$[$nout + 1]
+  
+done
+#wrfout.ocean_d2.2014043012.f00.0000
