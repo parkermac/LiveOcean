543d8ddc00300d159ccbb042b8d1b68b05e1733a
Parker MacCready <p.maccready@gmail.com>
2016-04-24 09:02:09 -0700

    hycom
    Updated exit number to 91.2

--- forcing/ocn/Ofun.py
+++ forcing/ocn/Ofun.py
@@ -16,7 +16,7 @@ def get_hycom_file_list():
     fn_list = []
     
     # get the xml of the catalog
-    xml_name = 'http://tds.hycom.org/thredds/catalog/GLBu0.08/expt_91.1/forecasts/catalog.xml'
+    xml_name = 'http://tds.hycom.org/thredds/catalog/GLBu0.08/expt_91.2/forecasts/catalog.xml'
     try:
         xfile = U.urlopen(xml_name, timeout=30)
     except:
