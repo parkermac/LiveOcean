527ffb08878270a334537b1612b72a80cabb6395
Parker MacCready <p.maccready@gmail.com>
2017-05-21 10:52:39 -0700

    check
    spiffing up check_all_results

--- driver/check_all_results.py
+++ driver/check_all_results.py
@@ -22,7 +22,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-g", "--gridname", type=str, default='cascadia1')
 parser.add_argument("-t", "--tag", type=str, default='base')
 parser.add_argument("-x", "--ex_name", type=str, default='lobio1')
-parser.add_argument("-nd", "--num_days", type=int, default=30)
+parser.add_argument("-nd", "--num_days", type=int, default=10)
 args = parser.parse_args()
 
 Ldir = Lfun.Lstart(args.gridname, args.tag)
@@ -40,27 +40,32 @@ for item in os.listdir(f_dir0):
     if item[0] == 'f' and len(item) == 11:
         f_dir0_list.append(item)
 f_dir0_list.sort()
-#f_dir0_list = f_dir0_list[-args.num_days:]
+f_dir0_list = f_dir0_list[-args.num_days:]
 
 force_dict = {'atm': ['lwrad_down.nc', 'Pair.nc', 'Qair.nc', 'rain.nc',
                       'swrad.nc', 'Tair.nc', 'Uwind.nc', 'Vwind.nc'],
               'ocn': ['ocean_bry.nc', 'ocean_clm.nc', 'ocean_ini.nc'],
               'ocn1': ['ocean_bry.nc', 'ocean_clm.nc', 'ocean_ini.nc'],
-              'riv': ['rivers.nc', ],
-              'tide': ['tides.nc', ]}
+              'riv': ['rivers.nc'],
+              'tide': ['tides.nc']}
 
-clist = ['atm', 'ocn', 'ocn1', 'riv', 'tide', 'dot_in', 'his', 'lp', 'azu']
+if 'bio' in args.ex_name:
+    force_dict['ocn'] = ['ocean_bry_bio.nc', 'ocean_clm_bio.nc', 'ocean_ini_bio.nc']
+    force_dict['ocn1'] = ['ocean_bry_bio.nc', 'ocean_clm_bio.nc', 'ocean_ini_bio.nc']
+    force_dict['riv'] = ['rivers_bio.nc']
+
+clist = ['atm', 'ocn', 'ocn1', 'riv', 'tide', 'dot_in', 'his', 'carbon', 'azu', 'low_pass', 'tracks']
 
 f_df = pd.DataFrame(index=f_dir0_list, columns=clist)
 
-for which_forecast in f_df.index[-args.num_days:]:
+for which_forecast in f_df.index:
     for which_force in force_dict.keys():
         force_dir = f_dir0 + which_forecast + '/' + which_force + '/'
         try:
             lll = os.listdir(force_dir)
             nc_list = force_dict[which_force]
             if set(nc_list).issubset(set(lll)):
-                f_df.ix[which_forecast, which_force] = 'YES'
+                f_df.loc[which_forecast, which_force] = 'YES'
                 if which_force in ['atm', 'ocn', 'ocn1']:
                     try:
                         time_format = '%Y.%m.%d %H:%M:%S'
@@ -69,14 +74,21 @@ for which_forecast in f_df.index[-args.num_days:]:
                         dt1 = datetime.strptime(ps['end_time'], time_format)
                         vdt0 = datetime.strptime(ps['var_start_time'], time_format)
                         vdt1 = datetime.strptime(ps['var_end_time'], time_format)
-                        f_df.ix[which_forecast, which_force] = str((vdt1-vdt0).days) + 'd'
+                        f_df.loc[which_forecast, which_force] = str((vdt1-vdt0).days) + 'd'
+                    except:
+                        pass
+                elif which_force in ['carbon', 'low_pass']:
+                    try:
+                        ps = Lfun.csv_to_dict(force_dir + 'Info/process_status.csv')
+                        if ps['result'] == 'success':
+                            f_df.loc[which_forecast, which_force] = 'YES'
                     except:
                         pass
             else:
-                f_df.ix[which_forecast, which_force] = 'no'
+                f_df.loc[which_forecast, which_force] = 'no'
         except:
             # assume the directory is missing
-            f_df.ix[which_forecast, which_force] = '--'
+            f_df.loc[which_forecast, which_force] = '--'
 
 # what forecasts have been run successfully
 r_dir0 = Ldir['roms'] + 'output/' + Ldir['gtagex'] + '/'
@@ -87,12 +99,13 @@ try:
             fl = os.listdir(r_dir0 + f_string)
 
             if 'liveocean.in' in fl:
-                f_df.ix[f_string, 'dot_in'] = 'YES'
-            if 'low_passed.nc' in fl:
-                f_df.ix[f_string, 'lp'] = 'YES'
+                f_df.loc[f_string, 'dot_in'] = 'YES'
+#            if 'low_passed.nc' in fl:
+#                # how can we check that it really ran?
+#                f_df.loc[f_string, 'lp'] = 'YES'
             flh = [x for x in fl if 'ocean_his' in x]
             flh.sort()
-            f_df.ix[f_string, 'his'] = str(int(flh[-1][10:14]))
+            f_df.loc[f_string, 'his'] = str(int(flh[-1][-7:-3]))
 except:
     pass
 
@@ -112,7 +125,7 @@ for f_string in f_df.index[-args.num_days:]:
         his_list = []
         for blob in blobs:
             his_list.append(blob.name)
-        f_df.ix[f_string, 'azu'] = str(int(his_list[-1][-6:-3]))
+        f_df.loc[f_string, 'azu'] = str(int(his_list[-1][-7:-3]))
     except:
         pass
 
