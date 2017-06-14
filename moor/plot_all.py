891d4e4948c2ce38540c4e8effb7bc8e1216c505
Parker MacCready <p.maccready@gmail.com>
2017-06-14 11:27:37 -0700

    moor
    spiffing up plot_all.py

--- moor/plot_all.py
+++ moor/plot_all.py
@@ -22,7 +22,7 @@ import Lfun
 Ldir = Lfun.Lstart()
 indir = Ldir['LOo'] + 'moor/'
 
-# choose the type of plot to make
+# choose the mooring extraction to plot
 print('\n%s\n' % '** Choose mooring file to plot **')
 m_list_raw = os.listdir(indir)
 m_list = []
@@ -60,60 +60,35 @@ for vv in ds.variables:
     elif ( ('ocean_time' in vdim) and ('s_w' in vdim) ):
         v3_list_w.append(vv)
 
-# load everything into a dict
-# (is this a good idea?)
+# create a dict into which to load everything
 V = dict()
-Vu = dict() # units
+# and a dict of units
+Vu = dict()
 
+#choose what to plot
 #list_to_plot = v3_list_rho + v3_list_w + v2_list
-if False:
-    list_to_plot = v3_list_rho
-    try:
-        list_to_plot.remove('CaCO3')
-    except ValueError:
-        pass
-else: # do it by hand
-    list_to_plot = ['u',
-     'v',
-     'temp',
-     'salt',
-     'NO3',
-     'phytoplankton',
-     'zooplankton',
-     'detritus',
-     'Ldetritus',
-     'oxygen',
-     'TIC',
-     'alkalinity',
-     'z_rho']#,
-     # 'PH',
-     # 'ARAG',
-     # 'z_rho']
+list_to_plot = v3_list_rho
 
 ltp = list_to_plot.copy()
-
 ltp.append('ocean_time')
-
+# gather data (including ocean_time and units
 for vv in ltp:
     V[vv] = ds[vv][:]
-    Vu[vv] = ds[vv].units
-        
-# an experiment about how much TIC change to expect from Ldetritus decomposition
-#ad_hoc = 500
-#V['dTIC'] = np.cumsum(V['Ldetritus'], axis=1)*.1*6.625 * ad_hoc
-#Vu['dTIC'] = Vu['TIC']
-#list_to_plot.append('dTIC')
+    try:
+        Vu[vv] = ds[vv].units
+    except AttributeError:
+        Vu[vv] = ''
 
 ds.close()
 
 #%% plotting
 
-ylim_dict = {'alkalinity':(2200, 2540)}
-
 NP = len(list_to_plot)
 
 NR = np.maximum(1, np.ceil(np.sqrt(NP)).astype(int))
 NC = np.ceil(np.sqrt(NP)).astype(int)
+if NR*NC - NC >= NP:
+    NR -= 1
 
 fig, axes = plt.subplots(nrows=NR, ncols=NC, figsize=(13,8), squeeze=False)
 
@@ -126,21 +101,30 @@ mdt = mdates.num2date(mdays) # list of datetimes of data
 dt_ticks = []
 dt_ticks_yr = []
 dt_ticklabels = []
-for yr in [2013, 2014, 2015, 2016]:
-    for mo in [1,4,7,10]:
-        dt = datetime(yr, mo, 1).date()
-        if dt > mdt[0].date() and dt < mdt[-1].date():
-            dt_ticks.append(dt)
-            if mo == 1:
-                dt_ticks_yr.append(dt)
-            dt_ticklabels.append(dt.strftime('%Y/%m'))
+yr0 = mdt[0].year
+yr1 = mdt[-1].year
+nyears = yr1 - yr0 + 1
+ndays = (mdt[-1] - mdt[0]).days
+do_ticks = False
+if nyears >= 2:
+    do_ticks = True
+    for yr in range(yr0, yr1+1):
+        for mo in [1,6]:
+            dt = datetime(yr, mo, 1).date()
+            if dt > mdt[0].date() and dt < mdt[-1].date():
+                dt_ticks.append(dt)
+                if mo == 1:
+                    dt_ticks_yr.append(dt)
+                dt_ticklabels.append(dt.strftime('%Y/%m'))
+else:
+    mdt = days
 
 cc = 0
-#nmid = round(V['salt'].shape[0]/2)
+nmid = round(V['salt'].shape[0]/2)
 N = V['salt'].shape[0]
 nbot = 0
-nmid = 3
-ntop = 7
+nmid = nmid
+ntop = N-1
 for vn in list_to_plot:
     ir = int(np.floor(cc/NC))
     ic = int(cc - NC*ir)
@@ -152,9 +136,8 @@ for vn in list_to_plot:
     elif V[vn].ndim == 1:
         ax.plot(mdt, V[vn])
 
-    if True:
-        # general case
-        ax.set_xlim(mdt[0], mdt[-1])
+    ax.set_xlim(mdt[0], mdt[-1])
+    if do_ticks:
         ax.set_xticks(dt_ticks)
         ax.set_xticklabels([])
         if ir == NR-1:
@@ -165,13 +148,8 @@ for vn in list_to_plot:
             ax.plot([dtyr, dtyr], aa, '-k')
         ax.set_ylim(aa)
     else:
-        # specific case
-        ax.set_xlim(datetime(2015,6,1), datetime(2015,8,31))
-        ax.grid()
-        try:
-            ax.set_ylim(ylim_dict[vn])
-        except:
-            pass
+        if ir == NR-1:
+            ax.set_xlabel('Days')
 
     ax.ticklabel_format(useOffset=False, axis='y')
     ax.text(.05, .85, vn,
@@ -182,7 +160,6 @@ for vn in list_to_plot:
             transform=ax.transAxes)
     cc += 1
 
-#fig.tight_layout()
 plt.suptitle(fn)
 
 plt.show()
