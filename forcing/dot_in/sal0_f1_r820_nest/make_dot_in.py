42efa81e1fdbfdc2ceb9d77dd1db4f7568d3d4af
Parker MacCready <p.maccready@gmail.com>
2017-06-16 11:43:00 -0700

    nesting
    updated the dot_in code to work with new driver_roms1.sh

--- forcing/dot_in/sal0_f1_r820_nest/make_dot_in.py
+++ forcing/dot_in/sal0_f1_r820_nest/make_dot_in.py
@@ -32,9 +32,15 @@ if Ldir['run_type'] == 'backfill':
 elif Ldir['run_type'] == 'forecast':
     days_to_run = float(Ldir['forecast_days'])
 
-dtsec = 4 # time step in seconds INTEGER (should fit evenly into 3600 sec)
+# time step in seconds INTEGER (should fit evenly into 3600 sec)
+if Ldir['blow_ups'] == 0:
+    dtsec = 10 
+elif Ldir['blow_ups'] == 1:
+    dtsec = 5
+else:
+    print('Unsupported number of blow ups: %d' % (Ldir['blow_ups']))
 restart_nrrec = '-1' # '-1' for a non-crash restart file, otherwise '1' or '2'
-his_interval = 36 # seconds to define and write to history files
+his_interval = 3600 # seconds to define and write to history files
 rst_interval = 1 # days between writing to the restart file (e.g. 5)
 
 zqt_height = '2.0d0'
@@ -45,8 +51,14 @@ zw_height = '10.0d0'
 # DERIVED VALUES
 
 if multi_core:
-    ntilei = '6' # number of tiles in I-direction (6)
-    ntilej = '12' # number of tiles in J-direction (12)
+    if Ldir['np_num'] == 72:
+        ntilei = '6' # number of tiles in I-direction (6)
+        ntilej = '12' # number of tiles in J-direction (12)
+    elif Ldir['np_num'] == 144:
+        ntilei = '8' # number of tiles in I-direction (6)
+        ntilej = '18' # number of tiles in J-direction (12)
+    else:
+        print('Unsupported number of processors: %d' % (Ldir['np_num']))
 else:
     ntilei = '1'
     ntilej = '1'
