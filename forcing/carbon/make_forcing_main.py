9e555e02e5b15d8c06c9fcd18e97c33c892c5c6d
Parker MacCready <p.maccready@gmail.com>
2017-07-20 17:33:37 -0700

    carbon
    bug fix

--- forcing/carbon/make_forcing_main.py
+++ forcing/carbon/make_forcing_main.py
@@ -27,10 +27,27 @@ his_list.sort()
 h_list = [int(his[-7:-3]) for his in his_list]
 
 nf = 15
+
 ii = 0
 h0 = h_list[ii*nf]
 h1 = h_list[(ii+1)*nf - 1]
 print('h0 = %d, h1 =%d' % (h0, h1))
+Ldir['h0'] = str(h0)
+Ldir['h1'] = str(h1)
+# run the code to create the forcing files
+func = ("make_forcing_worker(\'" +
+    Ldir['gridname'] + "\',\'" +
+    Ldir['tag'] + "\',\'" +
+    Ldir['date_string'] + "\',\'" +
+    Ldir['run_type'] + "\',\'" +
+    Ldir['indir'] + "\',\'" +
+    Ldir['h0'] + "\',\'" +
+    Ldir['h1'] + "\',\'" +
+    Ldir['LOogf_f'] + "\')")
+cmd = Ldir['which_matlab']
+run_cmd = [cmd, "-nodisplay", "-r", func, "&"]
+#subprocess.run(run_cmd)
+
 while h1 < h_list[-1]:
     ii += 1
     h0 = h1 + 1
@@ -53,7 +70,7 @@ while h1 < h_list[-1]:
         Ldir['LOogf_f'] + "\')")
     cmd = Ldir['which_matlab']
     run_cmd = [cmd, "-nodisplay", "-r", func, "&"]
-    subprocess.run(run_cmd)
+    #subprocess.run(run_cmd)
 
 # ************** END CASE-SPECIFIC CODE *****************
 
