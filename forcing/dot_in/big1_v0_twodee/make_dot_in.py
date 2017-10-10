6b46ea0e0d08a0327513fe53418a82271ccf52d8
parkermac <p.maccready@gmail.com>
2017-10-10 16:18:16 -0700

    big1
    more dt

--- forcing/dot_in/big1_v0_twodee/make_dot_in.py
+++ forcing/dot_in/big1_v0_twodee/make_dot_in.py
@@ -37,9 +37,9 @@ elif Ldir['run_type'] == 'forecast':
 
 # time step in seconds INTEGER (should fit evenly into 3600 sec)
 if Ldir['blow_ups'] == 0:
-    dtsec = 20 
+    dtsec = 10 
 elif Ldir['blow_ups'] == 1:
-    dtsec = 15
+    dtsec = 5
 else:
     print('Unsupported number of blow ups: %d' % (Ldir['blow_ups']))
 restart_nrrec = '-1' # '-1' for a non-crash restart file, otherwise '1' or '2'
