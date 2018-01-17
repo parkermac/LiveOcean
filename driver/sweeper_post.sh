f348d924249fcb68d502fb77a58ef03ed91b897f
parkermac <p.maccready@gmail.com>
2018-01-17 08:42:43 -0800

    sweeper
    update sweeper_post for new system

--- driver/sweeper_post.sh
+++ driver/sweeper_post.sh
@@ -3,16 +3,22 @@
 # Code to run all the post-processing jobs for a forecast
 # must be run drom LiveOcean/driver/
 
-# set a path and connect to a library of functions
-if [ $HOME = "/Users/pm7" ] ; then
-  LO_parent=$HOME"/Documents/LiveOcean"
-elif [ $HOME = "/home/parker" ] ; then
-  LO_parent="/data1/parker/LiveOcean"
+# run the code to put the environment into a csv
+if [ -e ../alpha/user_get_lo_info.sh ] ; then
+  ../alpha/user_get_lo_info.sh
+else
+  ../alpha/get_lo_info.sh
 fi
+
+# and read the csv into active variables
+while IFS=, read col1 col2
+do
+  eval $col1="$col2"
+done < ../alpha/lo_info.csv
 
 for frc in 'tracks_m' 'carbon' 'low_pass' 'ubc' 'surface' 'azu1'; do
 
-  ./driver_forcing1.sh -g cascadia1 -t base -x lobio5 -f $frc -r forecast > $LO_parent/driver/dlog_$frc &
+  ./driver_forcing2.sh -g cascadia1 -t base -x lobio5 -f $frc -r forecast > $LO"driver/dlog_"$frc &
   # Check that the job has finished successfully.
   PID1=$!
   wait $PID1
