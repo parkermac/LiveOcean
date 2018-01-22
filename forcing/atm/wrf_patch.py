0eb16bfab5fbb5177b10b75c0f3482cc96db2fa7
parkermac <p.maccready@gmail.com>
2018-01-22 07:09:46 -0800

    atm
    new crude code to patch a WRF day

---
+++ forcing/atm/wrf_patch.py
@@ -0,0 +1,41 @@
+"""
+Code to replace missing WRF files with those from another day.
+
+This will just copy all the files from the substitute day to
+the missing day, changing their names to be consistent with
+the missing day.  This does not change their internal times,
+but the cludgey way I make ROMS atm forcing files creates its
+own time variable so it does not matter.
+
+"""
+
+import os
+import shutil
+
+dir0="/pmr2/darr/wrf_crons/wrfout/"
+
+substitute_day = "20170325"
+
+missing_day =    "20170326"
+
+indir = dir0 + substitute_day + '/'
+outdir = dir0 + missing_day + '/'
+
+# I assume that outdir exists, is empty,
+# and that I have write permission there
+
+in_list = os.listdir(indir)
+
+for in_fn in in_list:
+    if in_fn[:5] == 'wrfout':
+        
+        out_fn = in_fn.replace(substitute_day, missing_day)
+        
+        if True: # Testing
+            print('Copying ' + indir+in_fn)
+            print('-- to - ' + outdir+out_fn)
+            
+        else:
+            shutil.copyfile(indir+in_fn, outdir+out_fn)
+
+
