9e3c4f4fedc0d399dba6e609a62b9f989f6ac188
parkermac <p.maccready@gmail.com>
2019-03-28 08:25:00 -0700

    Update extract_sections.py
    getting started with cas5

--- x_tef/extract_sections.py
+++ x_tef/extract_sections.py
@@ -33,11 +33,11 @@ reload(tef_fun)
 import argparse
 parser = argparse.ArgumentParser()
 # standard arguments
-parser.add_argument('-g', '--gridname', nargs='?', type=str, default='cas4')
-parser.add_argument('-t', '--tag', nargs='?', type=str, default='v2')
-parser.add_argument('-x', '--ex_name', nargs='?', type=str, default='lo6biom')
-parser.add_argument('-0', '--date_string0', nargs='?', type=str, default='2017.09.01')
-parser.add_argument('-1', '--date_string1', nargs='?', type=str, default='2017.09.03')
+parser.add_argument('-g', '--gridname', nargs='?', type=str, default='cas5')
+parser.add_argument('-t', '--tag', nargs='?', type=str, default='v3')
+parser.add_argument('-x', '--ex_name', nargs='?', type=str, default='lo8')
+parser.add_argument('-0', '--date_string0', nargs='?', type=str, default='2017.01.01')
+parser.add_argument('-1', '--date_string1', nargs='?', type=str, default='2017.01.03')
 # section specific arguments
 args = parser.parse_args()
 
