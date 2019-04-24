1e7bc2fba873f19d1b3a63bbe2e5a413fd802787
parkermac <p.maccready@gmail.com>
2019-04-24 10:47:06 -0700

    Update make_forcing_main.py

--- forcing/ocn4/make_forcing_main.py
+++ forcing/ocn4/make_forcing_main.py
@@ -156,7 +156,8 @@ elif (Ldir['run_type'] == 'backfill'):
         dt_next = dt_now - timedelta(days=counter)
         dts_next = datetime.strftime(dt_next, '%Y.%m.%d')
         try:
-            it0 = hnc_list.index('h' + dts_next + '.nc')
+            it00 = [i for i, s in enumerate(mylist) if dts_next in s]
+            it0 = it00[0]
             # note that "index" returns the index of the first match
             keep_looking = False
             if counter > 0:
