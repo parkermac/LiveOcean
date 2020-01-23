53c6ae152f0892a547ef8ad084b76760633d85c8
parkermac <p.maccready@gmail.com>
2020-01-23 13:43:11 -0800

    Update roms_plots.py
    questionable intervention

--- plotting/roms_plots.py
+++ plotting/roms_plots.py
@@ -3150,6 +3150,10 @@ def P_tracks_barber(in_dict):
     import trackfun as tf1#trackfun_1 as tf1
     import pickle
     fn = in_dict['fn']
+    
+    EI = Lfun.csv_to_dict(Ldir['LOo'] + 'tracks2/exp_info.csv')
+    EI['fn'] = fn
+    
     gtagex = fn.split('/')[-3]
     gtx_list = gtagex.split('_')
     import Lfun
