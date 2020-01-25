3c56b3d624e2d9a5c3456b386e89d10cf758e485
parkermac <p.maccready@gmail.com>
2020-01-25 10:59:36 -0800

    Update wrf_inventory.py
    complete rewrite

--- x_misc/wrf_inventory.py
+++ x_misc/wrf_inventory.py
@@ -11,93 +11,54 @@ import Lfun
 Ldir = Lfun.Lstart()
 
 import pandas as pd
+from datetime import datetime, timedelta
 
 if Ldir['lo_env'] == 'pm_mac':
-    f_dir0 = Ldir['parent'] + 'LiveOcean_data/wrf/'
+    f_dir0 = Ldir['data'] + 'wrf/'
 else: 
     f_dir0 = '/pmr2/darr/wrf_crons/wrfout/'
 
 # directory names are like 2012100700
 
-# expected directories
-from datetime import datetime, timedelta
-fe_list = []
-dt0 = datetime(2012,10,7)
-dt = dt0
-while datetime.now() - dt > timedelta(1):
-    fe_list.append(dt.strftime('%Y%m%d') + '00')
-    dt = dt + timedelta(1)
+#expected dates
+dte = pd.date_range(start=datetime(2012,10,7), end=datetime.now())
     
-clist = ['dir_exists','N_d2','N_d3','N_d4']
-f_df = pd.DataFrame(index = fe_list, columns = clist)
+clist = ['dir_name', 'dir_exists', 'd2', 'd3', 'd4']
 
-# what forecasts exist in the forcing directory
-f_list = []
-LL = os.listdir(f_dir0)
-LL.sort()
-for item in LL:
-    if item[-2:] == '00' and len(item) == 10:
-        f_list.append(item)
+f_df = pd.DataFrame(0, index = dte, columns = clist)
+
+for dt in dte:
+    f_df.loc[dt, 'dir_name'] = dt.strftime('%Y%m%d' + '00')
+
+# what 00 forecasts exist in the forcing directory
+f_list = os.listdir(f_dir0)
+f_list = [item for item in f_list if (item[-2:] == '00' and len(item) == 10)]
+f_list.sort()
+
+# list of datetime of forecasts that exist
+fdt_list = [datetime.strptime(item[:8],'%Y%m%d') for item in f_list]
         
-f_df['dir_exists'] = 'no'
+# note which dates we have forecast directories for
+f_df.loc[:,'dir_exists'] = 'no'
+f_df.loc[fdt_list, 'dir_exists'] = 'yes'
 
-for item in f_list:
-    if item in fe_list:
-        f_df.loc[item,'dir_exists'] = 'YES'
-        
-df_yes = f_df[f_df['dir_exists']=='YES']
-df_no = f_df[f_df['dir_exists']=='no']
-
-for item in df_yes.index:
-    fl = os.listdir(f_dir0 + item)
+for dt in fdt_list:
+    
+    dname = f_df.loc[dt, 'dir_name']
+    
+    fl = os.listdir(f_dir0 + dname)
     fl.sort()
-    # how many CONTINUOUS forecasts have been saved successfully
-    # file names are like wrfout.ocean_d2.2012100700.f00.0000
-    flag = 1
-    nd = 0
-    while flag == 1:
-        sd = ('00' + str(nd))[-2:]
-        if ('wrfout.ocean_d2.' + item + '.f' + sd + '.0000') in fl:
-            nd += 1
-        else:
-            flag = 0
-    df_yes.loc[item]['N_d2'] = nd
-       
-    flag = 1
-    nd = 0
-    while flag == 1:
-        sd = ('00' + str(nd))[-2:]
-        if ('wrfout.ocean_d3.' + item + '.f' + sd + '.0000') in fl:
-            nd += 1
-        else:
-            flag = 0
-    df_yes.loc[item]['N_d3'] = nd
     
-    flag = 1
-    nd = 0
-    while flag == 1:
-        sd = ('00' + str(nd))[-2:]
-        if ('wrfout.ocean_d4.' + item + '.f' + sd + '.0000') in fl:
-            nd += 1
-        else:
-            flag = 0
-    df_yes.loc[item]['N_d4'] = nd
+    n2 = len([item for item in fl if 'd2' in item])
+    n3 = len([item for item in fl if 'd3' in item])
+    n4 = len([item for item in fl if 'd4' in item])
     
-
-df_yes_but =  df_yes[(df_yes['N_d2'] < 25) | (df_yes['N_d3'] < 25) | (df_yes['N_d4'] < 25)]
-
-# make sure that the dates are in order
-df_yes_but = df_yes_but.sort_index()
-df_no = df_no.sort_index()
-
-# print to the screen
-pd.set_option('display.max_rows', 1000)
-print('MISSING DIRECTORIES')
-print(df_no)
-print('\nMISSING HOURS')
-print(df_yes_but)
-
-
+    f_df.loc[dt, 'd2'] = n2
+    f_df.loc[dt, 'd3'] = n3
+    f_df.loc[dt, 'd4'] = n4
+    
+f_df_no = f_df[f_df['dir_exists']=='no']
+f_df_yes = f_df[f_df['dir_exists']=='yes']
 
 
 
