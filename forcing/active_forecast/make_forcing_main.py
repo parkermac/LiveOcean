817776faedbbbcc385d03344bdde5eceb01907ff
Parker MacCready <p.maccready@gmail.com>
2020-09-17 11:03:47 -0700

    Update make_forcing_main.py
    finalizing use of P_DO

--- forcing/active_forecast/make_forcing_main.py
+++ forcing/active_forecast/make_forcing_main.py
@@ -15,7 +15,7 @@ Ldir, Lfun = ffun.intro()
 
 # ****************** CASE-SPECIFIC CODE *****************
 
-testing = True
+testing = False
 
 from datetime import datetime, timedelta
 start_time = datetime.now()
@@ -27,11 +27,8 @@ os.chdir(Ldir['LO'] + 'plotting/')
 
 # NOTE 2020.01.24: P_tracks_MERHAB is just for Ryan, it does not go to the website
 P_list_1 = ['P_tracks_MERHAB', 'P_merhab2', 'P_willapa_omega']
-P_list_2 = ['P_basic', 'P_Chl_DO', 'P_basic_salish', 'P_Chl_DO_salish', 'P_tracks_barber']
+P_list_2 = ['P_basic', 'P_DO', 'P_basic_salish', 'P_Chl_DO_salish', 'P_tracks_barber']
 P_list = P_list_1 + P_list_2
-
-if testing:
-    P_list = ['P_DO']
 
 result = 'success'
 for P_name in P_list:
