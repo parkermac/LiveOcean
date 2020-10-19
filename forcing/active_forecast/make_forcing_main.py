4ce1ce2b79a11f6d3e91a2f43432b1394383f2e8
Parker MacCready <p.maccready@gmail.com>
2020-10-19 08:20:01 -0700

    P_tracks_MERHAB
    got rid of this plotting job - no longer used

--- forcing/active_forecast/make_forcing_main.py
+++ forcing/active_forecast/make_forcing_main.py
@@ -25,8 +25,7 @@ from time import time
 print(' - Creating wesite images for ' + Ldir['date_string'])
 os.chdir(Ldir['LO'] + 'plotting/')
 
-# NOTE 2020.01.24: P_tracks_MERHAB is just for Ryan, it does not go to the website
-P_list_1 = ['P_tracks_MERHAB', 'P_merhab2', 'P_willapa_omega']
+P_list_1 = ['P_merhab2', 'P_willapa_omega']
 P_list_2 = ['P_basic', 'P_DO', 'P_basic_salish', 'P_Chl_DO_salish', 'P_tracks_barber']
 P_list = P_list_1 + P_list_2
 
@@ -38,7 +37,7 @@ for P_name in P_list:
     dstr0 = Ldir['date_string']
     dstr1 = Ldir['date_string']
     
-    if P_name in ['P_tracks_MERHAB', 'P_merhab2', 'P_tracks_barber']:
+    if P_name in ['P_merhab2', 'P_tracks_barber']:
         lt = 'merhab'
     elif P_name == 'P_DO':
         lt = 'daily_plus'
@@ -48,9 +47,6 @@ for P_name in P_list:
     else:
         lt = 'forecast'
         
-    # if testing:
-    #     lt = 'allhours'
-    
     # run the plotting code
     cmd = ['python','pan_plot.py',
                   '-g', Ldir['gridname'],'-t', Ldir['tag'],'-x', Ldir['ex_name'],
@@ -67,22 +63,12 @@ for P_name in P_list:
     input_filename = Ldir['LOo'] + 'plots/' + lt + '_' + P_name + '_' + Ldir['gtagex'] + '/movie.mp4'
     output_filename = P_name + '.mp4'
     
-    if P_name == 'P_tracks_MERHAB':
-        # send file to azure
-        print('\nCopying '+output_filename+' to azure')
-        container_name = 'active-forecast'
-        az_dict = Lfun.copy_to_azure(input_filename, output_filename, container_name, Ldir)
-        if az_dict['result'] == 'fail':
-            result = 'fail'
-            print('Failed to copy to Azure:')
-            print(output_filename)
-    else:
-        # send file to homer (only works from boiler)
-        print('\nCopying '+output_filename+' to homer')
-        cmd_list = ['scp',input_filename,
-            'pmacc@homer.u.washington.edu:/hw00/d47/pmacc/LO/Figs_active_forecast/'+output_filename]
-        ret = subprocess.call(cmd_list)
-        print('Return code = ' + str(ret) + ' (0=success)')
+    # send file to homer (only works from boiler)
+    print('\nCopying '+output_filename+' to homer')
+    cmd_list = ['scp',input_filename,
+        'pmacc@homer.u.washington.edu:/hw00/d47/pmacc/LO/Figs_active_forecast/'+output_filename]
+    ret = subprocess.call(cmd_list)
+    print('Return code = ' + str(ret) + ' (0=success)')
     
     print('  -- took %0.1f seconds' % (tt0-time()))
     
