36021e5c17b0ba0d7e1464ffc2584718d3750362
Parker MacCready <p.maccready@gmail.com>
2020-12-03 08:26:04 -0800

    Update forecast_cleaner.sh
    added the post-processing products to the list of things to be cleaned up

--- driver/forecast_cleaner.sh
+++ driver/forecast_cleaner.sh
@@ -4,8 +4,12 @@
 # Meant to be run by cron late every day, well after the files are needed.
 # Hard-wired only to work on boiler, with selected runs.
 
+# Also deletes some other post-processing products.
+
 rm /data1/parker/LiveOcean_roms/output/cas6_v3_lo8b/f*/ocean_his_002[6-9].nc
 rm /data1/parker/LiveOcean_roms/output/cas6_v3_lo8b/f*/ocean_his_00[3-7][0-9].nc
 
-# rm /data1/parker/LiveOcean_roms/output/sj0_v0_lo8nest/f*/ocean_his_002[6-9].nc
-# rm /data1/parker/LiveOcean_roms/output/sj0_v0_lo8nest/f*/ocean_his_00[3-7][0-9].nc
+rm /data1/parker/LiveOcean_roms/output/cas6_v3_lo8b/f*/cmop_*.nc
+rm /data1/parker/LiveOcean_roms/output/cas6_v3_lo8b/f*/ocean_layers.nc
+rm /data1/parker/LiveOcean_roms/output/cas6_v3_lo8b/f*/ocean_surface.nc
+rm /data1/parker/LiveOcean_roms/output/cas6_v3_lo8b/f*/low_passed_UBC.nc
