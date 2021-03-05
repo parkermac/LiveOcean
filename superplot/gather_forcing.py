3c1b6768c38706bec4a8ba199e7c2d7e98e42479
Parker MacCready <p.maccready@gmail.com>
2021-03-05 13:28:32 -0800

    new superplot_salt

--- superplot/gather_forcing.py
+++ superplot/gather_forcing.py
@@ -23,9 +23,9 @@ import matplotlib.pyplot as plt
 outdir = Ldir['LOo'] + 'superplot/'
 Lfun.make_dir(outdir)
 
-gtag = 'cas4_v2'
-gtagex = gtag + '_lo6biom'
-year = '2017'
+gtag = 'cas6_v3'
+gtagex = gtag + '_lo8b'
+year = '2019'
 
 # River forcing
 # created by x_river/extract_rivers.py
@@ -35,26 +35,26 @@ riv_df0 = pd.read_pickle(fn)
 # keep only a subset of the rivers
 riv_df = riv_df0.loc[:, ['columbia', 'fraser', 'skagit']]
 
-# Tide
-pth = os.path.abspath(Ldir['parent'] + 'ptools/tide_obs_mod/')
-if pth not in sys.path:
-    sys.path.append(pth)
-import obsfun
-noaa_sn_dict, dfo_sn_dict, sn_dict = obsfun.get_sn_dicts()
-t_indir = Ldir['parent'] + 'ptools_output/tide/mod_data/' + gtagex + '/'
-t_fn = t_indir + 'tide_' + str(sn_dict['Seattle']) + '_' + year + '.p'
-tide_df = pickle.load(open(t_fn, 'rb'))
-# remove the timezone
-tide_df = tide_df.tz_localize(None)
-# make a dataframe of spring-neap conditions
-eta = tide_df['eta'].values
-eta_rms = np.sqrt(zfun.filt_godin(eta**2))
-tide_df['eta_rms'] = eta_rms
-# subsample to daily
-tide_daily_df = tide_df.loc[::24, 'eta_rms']
+# # Tide
+# pth = os.path.abspath(Ldir['parent'] + 'ptools/tide_obs_mod/')
+# if pth not in sys.path:
+#     sys.path.append(pth)
+# import obsfun
+# noaa_sn_dict, dfo_sn_dict, sn_dict = obsfun.get_sn_dicts()
+# t_indir = Ldir['parent'] + 'ptools_output/tide/mod_data/' + gtagex + '/'
+# t_fn = t_indir + 'tide_' + str(sn_dict['Seattle']) + '_' + year + '.p'
+# tide_df = pickle.load(open(t_fn, 'rb'))
+# # remove the timezone
+# tide_df = tide_df.tz_localize(None)
+# # make a dataframe of spring-neap conditions
+# eta = tide_df['eta'].values
+# eta_rms = np.sqrt(zfun.filt_godin(eta**2))
+# tide_df['eta_rms'] = eta_rms
+# # subsample to daily
+# tide_daily_df = tide_df.loc[::24, 'eta_rms']
 
 # Wind
-fnw = Ldir['LOo'] + 'moor/' + gtagex + '_2017.01.01_2018.11.29/NANOOS_ChaBa_Buoy_hourly.nc'
+fnw = Ldir['LOo'] + 'moor/custom_' + gtagex + '_'+year+'.01.01_'+year+'.12.31/shelf_hourly.nc'
 moor_ds = nc.Dataset(fnw)
 ot = moor_ds['ocean_time'][:]
 svstr = moor_ds['svstr'][:]
@@ -67,8 +67,16 @@ wind_df['svstr'] = svstr
 wind_df['svstr_lp'] = svstr_lp
 wind_df = wind_df.resample('D').mean()
 
+# Tide
+# make a dataframe of spring-neap conditions
+eta = zfun.fillit(moor_ds['zeta'][:])
+eta_rms = np.sqrt(zfun.filt_godin(eta**2))
+# subsample to daily
+tide_daily_df = pd.DataFrame(index=wind_df.index, columns=['eta_rms'])
+tide_daily_df['eta_rms'] = eta_rms[::24]
+
 # combine daily fields into a single DataFrame
-comb_df = pd.DataFrame(index=pd.date_range(start='1/1/2017', end='12/31/2017'))
+comb_df = pd.DataFrame(index=pd.date_range(start='1/1/'+year, end='12/31/'+year))
 comb_df['RMS Tide Height (m)'] = tide_daily_df
 comb_df['8-day NS Wind Stress (Pa)'] = wind_df['svstr_lp']
 comb_df['Columbia R. Flow (1000 m3/s)'] = riv_df['columbia']/1000
