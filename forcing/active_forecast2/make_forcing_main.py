01639aedfe9a99ba0273e4c592529b0cb8a01c29
Parker MacCready <p.maccready@gmail.com>
2021-09-08 10:33:31 -0700

    Update make_forcing_main.py
    trying to fig scp to homer

--- forcing/active_forecast2/make_forcing_main.py
+++ forcing/active_forecast2/make_forcing_main.py
@@ -35,7 +35,7 @@ dt00 = datetime.strptime(ds00,'%Y.%m.%d')
     
 procs = []
 
-if Ldir['testing'] == True:
+if False:#Ldir['testing'] == True:
     moviename_list = ['P1_nshelf_oxygen_bot']
 else:
     moviename_list = ['P1_full_salt_top', 'P1_full_oxygen_bot', 'P1_nshelf_oxygen_bot',
@@ -45,55 +45,56 @@ else:
                     
 os.chdir(Ldir['LO'] + 'plot5/')
 
-tt0 = time()
-result = 'success'
-for moviename in moviename_list:
-    (pt,dom,vn,BOT) = moviename.split('_')
-    tracks = 'False'
-    emask = 'False'
-    avl = 'True'
-    bot = 'False'
-    if BOT == 'bot':
-        bot = 'True'
-    if vn in ['oxygen', 'ARAG', 'speed']:
-        avl = 'False'
-    if (vn in ['oxygen', 'ARAG']) and (dom == 'full'):
-        emask = 'True'
-    if vn == 'salt':
-        tracks = 'True'
+if Ldir['testing'] == False:
+    tt0 = time()
+    result = 'success'
+    for moviename in moviename_list:
+        (pt,dom,vn,BOT) = moviename.split('_')
+        tracks = 'False'
+        emask = 'False'
+        avl = 'True'
+        bot = 'False'
+        if BOT == 'bot':
+            bot = 'True'
+        if vn in ['oxygen', 'ARAG', 'speed']:
+            avl = 'False'
+        if (vn in ['oxygen', 'ARAG']) and (dom == 'full'):
+            emask = 'True'
+        if vn == 'salt':
+            tracks = 'True'
         
-    if moviename == 'Phab_full_salt_top':
-        ttag = 'hab'
-    else:
-        ttag = 'base'
+        if moviename == 'Phab_full_salt_top':
+            ttag = 'hab'
+        else:
+            ttag = 'base'
         
-    if moviename == 'P1_nshelf_oxygen_bot':
-        # start this one a couple day s earlier
-        dt0 = dt00 - timedelta(days=2) # start earlier
-        dt1 = dt00 + timedelta(days=Ldir['forecast_days']-1)
-    else:
-        dt0 = dt00
-        dt1 = dt0 + timedelta(days=Ldir['forecast_days']-1)
-    ds0 = dt0.strftime('%Y.%m.%d')
-    ds1 = dt1.strftime('%Y.%m.%d')
+        if moviename == 'P1_nshelf_oxygen_bot':
+            # start this one a couple day s earlier
+            dt0 = dt00 - timedelta(days=2) # start earlier
+            dt1 = dt00 + timedelta(days=Ldir['forecast_days']-1)
+        else:
+            dt0 = dt00
+            dt1 = dt0 + timedelta(days=Ldir['forecast_days']-1)
+        ds0 = dt0.strftime('%Y.%m.%d')
+        ds1 = dt1.strftime('%Y.%m.%d')
         
-    cmd = ['python', 'p5.py', '-ds0', ds0, '-ds1', ds1, '-lt', 'hourly', '-mov', 'True',
-        '-pt', pt,
-        '-dom', dom, '-vn', vn, '-tracks', tracks, '-emask', emask, '-ttag', ttag,
-        '-avl', avl, '-bot', bot]
+        cmd = ['python', 'p5.py', '-ds0', ds0, '-ds1', ds1, '-lt', 'hourly', '-mov', 'True',
+            '-pt', pt,
+            '-dom', dom, '-vn', vn, '-tracks', tracks, '-emask', emask, '-ttag', ttag,
+            '-avl', avl, '-bot', bot]
         
-    print('\n' + moviename)
-    sys.stdout.flush()
-    sleep(4)
-    proc = subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    procs.append(proc)
+        print('\n' + moviename)
+        sys.stdout.flush()
+        sleep(4)
+        proc = subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        procs.append(proc)
 
-for proc in procs:
-    # note "error" appears to be where the ffmpeg screen output goes
-    output, errors = proc.communicate()
+    for proc in procs:
+        # note "error" appears to be where the ffmpeg screen output goes
+        output, errors = proc.communicate()
     
-print('time to run all jobs = %0.1f sec' % (time() - tt0))
-sys.stdout.flush()
+    print('time to run all jobs = %0.1f sec' % (time() - tt0))
+    sys.stdout.flush()
 
 alt_outdir = Ldir['LOo'] + 'Figs_active_forecast/'
 Lfun.make_dir(alt_outdir, clean=True)
@@ -103,7 +104,7 @@ for moviename in moviename_list:
     input_filename = Ldir['LOo'] + 'p5/' + Ldir['gtagex'] + '/' + moviename + '/' + moviename + '.mp4'
     output_filename = moviename + '.mp4'
 
-    if Ldir['testing'] == False:
+    if True:#Ldir['testing'] == False:
         sleep(4)
         # send file to homer (only works from boiler)
         print('\nCopying '+output_filename+' to homer')
@@ -111,11 +112,22 @@ for moviename in moviename_list:
         try:
             cmd_list = ['scp',input_filename,
                 'pmacc@homer.u.washington.edu:/hw00/d47/pmacc/LO/Figs_active_forecast/'+output_filename]
-            ret = subprocess.call(cmd_list)
-            if ret != 0:
-                print('WARNING: problem with movie ' + moviename)
-                result = 'fail'
-            print('Return code = ' + str(ret) + ' (0=success)')
+                
+            proc = subprocess.Popen(cmd_list,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            # note "error" appears to be where the ffmpeg screen output goes
+            stdout, stderr = proc.communicate()
+            if len(stdout) > 0:
+                print(' sdtout '.center(60,'-'))
+                print(stdout.decode())
+            if len(stderr) > 0:
+                print(' stderr '.center(60,'-'))
+                print(stderr.decode())
+            sys.stdout.flush()
+            # ret = subprocess.call(cmd_list)
+            # if ret != 0:
+            #     print('WARNING: problem with movie ' + moviename)
+            #     result = 'fail'
+            # print('Return code = ' + str(ret) + ' (0=success)')
         except Exception as e:
             print(e)
             pass
